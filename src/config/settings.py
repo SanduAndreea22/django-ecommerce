a0ffdd129ba413071e20d0e4b94af010cad88e9c
Andreea <luiza.deea22@gmail.com>
2026-01-13 22:53:35 +0200

    deployed

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,11 +1,9 @@
 """
 Django settings for Cash-only eCommerce (test with real email).
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
-from .secrets import SECRET_KEY, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # -------------------------------
 # Paths
@@ -15,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------
 # Security
 # -------------------------------
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'cheie-temporara-pt-local')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -164,17 +163,17 @@ MESSAGE_TAGS = {
 # -------------------------------
 # Email (Real Gmail for test)
 # -------------------------------
+#EMAIL_HOST_USER = EMAIL_HOST_USER
+#EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+# DEFAULT_FROM_EMAIL = f'CashOnly eCommerce <{EMAIL_HOST_USER}>'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-#EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-# DEFAULT_FROM_EMAIL = f'CashOnly eCommerce <{EMAIL_HOST_USER}>'
-DEFAULT_FROM_EMAIL = f'CashOnly eCommerce <{EMAIL_HOST_USER}>'
-
+DEFAULT_FROM_EMAIL = f"CashOnly eCommerce <{os.environ.get('EMAIL_HOST_USER')}>"
 # -------------------------------
 # Default primary key field type
 # -------------------------------
